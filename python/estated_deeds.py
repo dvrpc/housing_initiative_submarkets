7084c90f043fee38ddabac74f8f0d9ff762c2e2c
bcarney <bcarney1919@gmail.com>
2022-05-11 14:25:32 -0400

    Updated estated_deeds.py

--- python/estated_deeds.py
+++ python/estated_deeds.py
@@ -30,13 +30,9 @@ print(
 
 distressed_sale_loan_type = distressed_sale[["loan_type"]]
 distressed_sale_loan_type["count"] = ""
-distressed_sale_loan_type_grouped = distressed_sale_loan_type.groupby(
-    "loan_type"
-).count()
+distressed_sale_loan_type_grouped = distressed_sale_loan_type.groupby("loan_type").count()
 print(distressed_sale_loan_type_grouped)
-print(
-    len(distressed_sale_loan_type.loc[distressed_sale_loan_type["loan_type"].isnull()])
-)
+print(len(distressed_sale_loan_type.loc[distressed_sale_loan_type["loan_type"].isnull()]))
 # Note: Count does not match up. Count is much lower when grouping by loan_type than simply getting the count of distressed_sale transactions. It's possible that a lot of the loan_type values are null.
 # Note: Confirmed. The loan_type is null for 3529 records--nearly all of the distressed_sale subset of the deeds data.
 
@@ -57,7 +53,7 @@ print(
 
 # Cash Sales
 cash_sales = deeds.loc[
-    (deeds["loan_type"] == "CASH") | (deeds["document_type"] == "CASH SALE")
+    (deeds["loan_type"] == "CASH") | (deeds["document_type"] == "CASH SALE DEED")
 ]
 cash_sales_rows = len(cash_sales)
 print(
