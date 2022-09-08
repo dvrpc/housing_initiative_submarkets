5b68d313f2d50b54e3ec73f473ae6ef487133a8f
brcarney <bcarney1919@gmail.com>
2022-09-08 11:44:20 -0400

    Created tran_type lookup table

---
+++ sql/tran_type.sql
@@ -0,0 +1,14 @@
+INSERT INTO twg.tran_type
+VALUES
+('1', '1 or 6',	'Non-Arms length transactions with valid TITLE COMPANY NAME (no accomodations)'),
+('2', '1 or 6',	'Arms-length Residential transactions (Purchase/Resales)'),
+('3', '1 or 6',	'New Construction Residential transactions'),
+('4', '1 or 6',	'REO and Trustee Deeds'),
+('5', '1 or 6',	'Insured NON-Residential Grant Deeds'),
+('1', '2 or 7',	'HELOCS - both Purchase Money and Non-Purchase Money HELOCS'),
+('2', '2 or 7',	'REFI LOANS and 2ND TRUST DEEDS (purchase and non-purchase money trust deeds)'),
+('3', '2 or 7',	'RESALE PURCHASE MONEY LOANS (Subset of TRANSACTION TYPE 2)'),
+('4', '2 or 7',	'CONSTRUCTION LOANS  both Purchase Money and Non-Purchase Money Loans (LOAN TYPE "B")'),
+('5', '2 or 7',	'NEW RESIDENTIAL PURCHASE MONEY LOANS  - Subset of TRANSACTION TYPE 3'),
+('6', '2 or 7',	'NON-RESIDENTIAL Mortgages (Purchase & non-purchase)')
+;
