08000c8549557d73006cd2aa7c27fbb2ef10b1cd
brcarney <bcarney1919@gmail.com>
2022-12-27 14:36:38 -0500

    Updated script
    Added variables to overlay df

--- R/PullCensusData_SubmarketOverlays.R
+++ R/PullCensusData_SubmarketOverlays.R
@@ -1,5 +1,5 @@
 # Author: Brian Carney
-# Last updated: 12/22/2022
+# Last updated: 12/27/2022
 # Prepared for: Region Housing Initiative - Submarket Clustering Analysis Overlays
 
 # Set working directory
@@ -43,9 +43,14 @@ acs5_varlist_2020 <- load_variables(year = 2020,
 acs5_20_overlay_vars <- c(
   # Aggregate Number of Vehicles Available
   AGG_NUM_VEH = "B25046_001",
-  # Means of Transportation to Work by Vehicles Available
-  COMM_TOTHH = "B08141_001",
-  COMM_PUBT = "B08141_016",
+  # Means of Transportation to Work
+  COMM_TOTHH = "B08101_001",
+  COMM_DRVAL = "B08101_009",
+  COMM_CARPL = "B08101_017",
+  COMM_PUBT = "B08101_025",
+  COMM_WALK = "B08101_033",
+  COMM_OTHR = "B08101_041",
+  COMM_WFHO = "B08101_049",
   # Educational Attainment
   EDU_TOTL = "B15003_001",
   EDU_NONE = "B15003_002",
@@ -74,6 +79,7 @@ acs5_20_overlay_vars <- c(
   EDU_DOCT = "B15003_025",
   # Housing Cost Burden
   ## Gross Rent as a Percentage of Household Income (Renter-Occupied Housing Units)
+  RPI_TOTPOP = "B25070_001",
   RPI_00to09 = "B25070_002",
   RPI_10to14 = "B25070_003",
   RPI_15to19 = "B25070_004",
@@ -85,6 +91,7 @@ acs5_20_overlay_vars <- c(
   RPI_50more = "B25070_010",
   RPI_NA = "B25070_011",
   ## Selected Monthly Owner Costs by Mortgage Status (Owner-Occupied Housing Units)
+  OPI_MORT_POP = "B25091_002",
   OPI_00to09 = "B25091_003",
   OPI_10to14 = "B25091_004",
   OPI_15to19 = "B25091_005",
@@ -108,8 +115,11 @@ acs5_20_overlay_vars <- c(
   POP_WNH = "B01001H_001",
   POP_LAT = "B01001I_001",
   # Unemployment
-  EMP_POP = "B23025_001",
-  EMP_UNEMP = "B23025_005"
+  EMP_POPU = "B23025_001",
+  EMP_LABF = "B23025_002",
+  EMP_CVLF = "B23025_003",
+  EMP_UNEM = "B23025_005",
+  EMP_ARMF = "B23025_006"
   )
 
 dvrpc_states <- c(34, 42)
@@ -130,3 +140,53 @@ raw_data <- get_acs(geography = "tract",
 
 # Import Submarket Results
 tracts_submarkets <- read.csv("U:\\FY2022\\Planning\\RegionalHousingInitiative\\SubmarketAnalysis\\data\\LPA_Test3_Submarkets\\tracts_submarkets.csv", row.names = "X")
+
+
+# Overlay Variables
+overlay_df <- raw_data %>%
+  mutate(CARS_HH = round(AGG_NUM_VEH/COMM_TOTHH, 1)) %>%  # Cars per HH
+  mutate(CS_PUBT = round(100 * (COMM_PUBT/COMM_TOTHH), 1)) %>% # Percent of HHs that commute to work via public transit
+  mutate(CS_DRIVE = round((100 * (COMM_DRVAL + COMM_CARPL)/COMM_TOTHH), 1)) %>% # Percent of HHs that commute to work via driving
+  mutate(CS_WFH = round(100 * (COMM_WFHO/COMM_TOTHH), 1)) %>% # Percent of HHs that work from home
+  mutate(ED_LHSC = round(100 *((EDU_NURS +
+                         EDU_KIND +
+                         EDU_FRST +
+                         EDU_SCND +
+                         EDU_THRD +
+                         EDU_FRTH +
+                         EDU_FFTH +
+                         EDU_SXTH +
+                         EDU_SVTH +
+                         EDU_EGTH +
+                         EDU_NNTH +
+                         EDU_TNTH +
+                         EDU_ELTH +
+                         EDU_TWTH)/EDU_TOTL), 1)) %>%
+  mutate(ED_HSCH = round(100 * ((EDU_HSCH + EDU_GEDE)/EDU_TOTL), 1)) %>%
+  mutate(ED_BACH = round(100 *((EDU_BACH +
+                          EDU_MAST +
+                          EDU_PROF +
+                          EDU_DOCT)/EDU_TOTL), 1)) %>%
+  mutate(HC_BURD = round(100 * ((RPI_30to34 +
+                          RPI_35to39 +
+                          RPI_40to49 +
+                          RPI_50more +
+                          OPI_30to34 +
+                          OPI_35to39 +
+                          OPI_40to49 +
+                          OPI_50more)
+                          /(OPI_MORT_POP + RPI_TOTPOP)), 1)) %>%
+  mutate(HC_SEVB = round(100 * ((RPI_50more +
+                            OPI_50more)
+                         /(OPI_MORT_POP + RPI_TOTPOP)), 1)) %>%
+  mutate(PCT_WHI = round(100 * (POP_WHI/POP_TOT), 1)) %>%
+  mutate(PCT_BLK = round(100 * (POP_BLK/POP_TOT), 1)) %>%
+  mutate(PCT_AIA = round(100 * (POP_AIA/POP_TOT), 1)) %>%
+  mutate(PCT_ASN = round(100 * (POP_ASN/POP_TOT), 1)) %>%
+  mutate(PCT_HPI = round(100 * (POP_HPI/POP_TOT), 1)) %>%
+  mutate(PCT_OTH = round(100 * (POP_OTH/POP_TOT), 1)) %>%
+  mutate(PCT_TWO = round(100 * (POP_TWO/POP_TOT), 1)) %>%
+  mutate(PCT_WNH = round(100 * (POP_WNH/POP_TOT), 1)) %>%
+  mutate(PCT_LAT = round(100 * (POP_LAT/POP_TOT), 1)) %>%
+  mutate(UNEMP_RT = round(100 * (EMP_UNEM/EMP_CVLF), 1)) %>%
+  select(GEOID, CARS_HH, CS_PUBT, CS_DRIVE, CS_WFH, ED_LHSC, ED_HSCH, ED_BACH, HC_BURD, HC_SEVB, PCT_WHI, PCT_BLK, PCT_AIA, PCT_ASN, PCT_HPI, PCT_OTH, PCT_TWO, PCT_WNH, PCT_LAT, UNEMP_RT)
