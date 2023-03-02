48003d09b61afc5dd0515559c6ca625ccfb42b7e
brcarney <bcarney1919@gmail.com>
2023-03-02 08:40:29 -0500

    updated scripts: twg_deeds_2016, twg_deeds_2021

--- sql/twg_deeds_2016.sql
+++ sql/twg_deeds_2016.sql
@@ -1,3 +1,6 @@
 drop table if exists twg.twg_deeds_2016;
 create table twg.twg_deeds_2016 as
 select * from twg_raw.twg_deeds_2016 td;
+UPDATE twg.twg_deeds_2016
+SET
+"PROPID" = REPLACE("PROPID", 'TRAN', 'PROP');
--- sql/twg_deeds_2021.sql
+++ sql/twg_deeds_2021.sql
@@ -1,3 +1,6 @@
 drop table if exists twg.twg_deeds_2021;
 create table twg.twg_deeds_2021 as
 select * from twg_raw.twg_deeds_2021 td;
+UPDATE twg.twg_deeds_2021
+SET
+"PROPID" = REPLACE("PROPID", 'TRAN', 'PROP');
