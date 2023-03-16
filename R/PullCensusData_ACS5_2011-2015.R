be4d8ea0fbcd51decf048d0c7fb0c5b59b7a5fe2
brcarney <bcarney1919@gmail.com>
2023-03-16 13:29:14 -0400

    edited 2011-2015 script; added county script

--- R/PullCensusData_ACS5_2011-2015.R
+++ R/PullCensusData_ACS5_2011-2015.R
@@ -1,5 +1,5 @@
 # Author: Brian Carney
-# Last updated: 03/13/2023
+# Last updated: 03/16/2023
 # Prepared for: Region Housing Initiative - Submarket Clustering Analysis
 
 # Set working directory
@@ -27,14 +27,14 @@ library(rgdal)
 library(tidycensus)
 library(censusapi)
 
-#Load Census API key
+# Load Census API key
 census_api_key("e6cd8f90ccb0acacdfa5373911a2e73b96dbd792", install=TRUE, overwrite=TRUE)
 readRenviron("~/.Renviron")
 
-#Region FIPS
+# Region FIPS
 fips_region <- c("34005", "34007", "34015", "34021", "42017", "42029", "42045", "42091", "42101")
 
-#Variable List - For Reference
+# Variable List - For Reference
 acs5_varlist_2015 <- load_variables(year = 2015,
                                     dataset = "acs5",
                                     cache = TRUE)
@@ -182,7 +182,7 @@ acs5_15_vars_tot <- c(
 )
 
 dvrpc_states <- c(34, 42)
-dvrpc_fips <- c('34005|34007|34015|34021|42017|42029|42045|42091|42101')
+dvrpc_fips <- c('^34005|^34007|^34015|^34021|^42017|^42029|^42045|^42091|^42101')
 
 raw_data_15 <- get_acs(geography = "block group",
                     variables = acs5_15_vars_tot,
@@ -207,25 +207,39 @@ raw_data_15_crosswalk <- merge(raw_data_15, crosswalk, by.x="GEOID_10", by.y="bg
 
 # Summarize data to 2020 tracts
 acs5_15_2020tracts <- raw_data_15_crosswalk %>%
-  select(POP_TOT, HH_TOT, UNITS_TOT, tr2020ge, wt_pop, wt_hh, wt_hu) %>%
-  mutate(POP_TOT_CALC = round(POP_TOT * wt_pop, 0)) %>%
-  mutate(HH_TOT_CALC = round(HH_TOT * wt_hh, 0)) %>%
-  mutate(UNITS_TOT_CALC = round(UNITS_TOT * wt_hu, 0)) %>%
-  select(tr2020ge, POP_TOT_CALC, HH_TOT_CALC, UNITS_TOT_CALC) %>%
+  mutate(POP_TOT_15 = round(POP_TOT * wt_pop, 0)) %>%
+  mutate(HH_TOT_15 = round(HH_TOT * wt_hh, 0)) %>%
+  mutate(UNITS_TOT_15 = round(UNITS_TOT * wt_hu, 0)) %>%
+  mutate(UNIT_1DET_15 = round(UNIT_1DET * wt_hu, 0)) %>%
+  mutate(UNIT_1ATT_15 = round(UNIT_1ATT * wt_hu, 0)) %>%
+  mutate(UNIT_2_15 = round(UNIT_2 * wt_hu, 0)) %>%
+  mutate(UNIT_3or4_15 = round(UNIT_3or4 * wt_hu, 0)) %>%
+  mutate(UNIT_5to9_15 = round(UNIT_5to9 * wt_hu, 0)) %>%
+  mutate(UNIT_1019_15 = round(UNIT_1019 * wt_hu, 0)) %>%
+  mutate(UNIT_2049_15 = round(UNIT_2049 * wt_hu, 0)) %>%
+  mutate(UNIT_50P_15 = round(UNIT_50P * wt_hu, 0)) %>%
+  select(tr2020ge, POP_TOT_15, HH_TOT_15, UNITS_TOT_15, UNIT_1DET_15, UNIT_1ATT_15, UNIT_2_15, UNIT_3or4_15, UNIT_5to9_15, UNIT_1019_15, UNIT_2049_15, UNIT_50P_15) %>%
   group_by(tr2020ge) %>%
   summarise(across(everything(), sum)) %>%
-  rename("GEOID" = "tr2020ge") %>%
-  rename("POP_TOT_15" = "POP_TOT_CALC") %>%
-  rename("HH_TOT_15" = "HH_TOT_CALC") %>%
-  rename("UNITS_TOT_15" = "UNITS_TOT_CALC")
+  rename("GEOID" = "tr2020ge")
 
 
-# Import 2020 dataset
-tracts_2020 <- read.csv("U:\\FY2022\\Planning\\RegionalHousingInitiative\\SubmarketAnalysis\\data\\acs5_2020_variables.csv", colClasses = c("GEOID"="character"))
-
+# Import 2020 data
+raw_data_20 <- get_acs(geography = "tract",
+                       variables = acs5_15_vars_tot,
+                       year = 2020,
+                       state = dvrpc_states,
+                       survey = "acs5",
+                       output = "wide"
+) %>%
+  mutate(year = 2020) %>%
+  filter(str_detect(GEOID, dvrpc_fips)) %>%
+  select(GEOID, year, ends_with("E"), -NAME)%>%
+  `colnames<-`(str_replace(colnames(.),"E$","")) 
 
 # Join datasets
-check_df <- left_join(tracts_2020, acs5_15_2020tracts, by="GEOID")
+check_df <- left_join(raw_data_20, acs5_15_2020tracts, by="GEOID")
+
 
 # Import tracts with submarkets
 tracts_submarkets <- read.csv("U:\\FY2022\\Planning\\RegionalHousingInitiative\\SubmarketAnalysis\\data\\LPA_Test3_Submarkets\\lpa_results_3_submarkets.csv", colClasses = c("X" = "character")) %>%
@@ -234,9 +248,19 @@ tracts_submarkets <- read.csv("U:\\FY2022\\Planning\\RegionalHousingInitiative\\
 
 # Join datasets
 change_df <- left_join(tracts_submarkets, check_df, by="GEOID") %>%
-  select(Class, POP_TOT, HH_TOT, UNITS_TOT, POP_TOT_15, HH_TOT_15, UNITS_TOT_15) %>%
+  select(Class, POP_TOT, POP_TOT_15, HH_TOT, HH_TOT_15, UNITS_TOT, UNITS_TOT_15, UNIT_1DET, UNIT_1DET_15, UNIT_1ATT, UNIT_1ATT_15, UNIT_2, UNIT_2_15, UNIT_3or4, UNIT_3or4_15, UNIT_5to9, UNIT_5to9_15, UNIT_1019, UNIT_1019_15, UNIT_2049, UNIT_2049_15, UNIT_50P, UNIT_50P_15) %>%
   group_by(Class) %>%
   summarise(across(everything(), sum)) %>%
   mutate(POP_TOT_CHG = round(100 * ((POP_TOT - POP_TOT_15)/POP_TOT_15), 1)) %>%
   mutate(HH_TOT_CHG = round(100 * ((HH_TOT - HH_TOT_15)/HH_TOT_15), 1)) %>%
-  mutate(UNITS_TOT_CHG = round(100 * ((UNITS_TOT - UNITS_TOT_15)/UNITS_TOT_15), 1))
+  mutate(UNIT_1DET_CHG = round(100 * ((UNIT_1DET - UNIT_1DET_15)/UNIT_1DET_15), 1)) %>%
+  mutate(UNIT_1ATT_CHG = round(100 * ((UNIT_1ATT - UNIT_1ATT_15)/UNIT_1ATT_15), 1)) %>%
+  mutate(UNIT_2_CHG = round(100 * ((UNIT_2 - UNIT_2_15)/UNIT_2_15), 1)) %>%
+  mutate(UNIT_3or4_CHG = round(100 * ((UNIT_3or4 - UNIT_3or4_15)/UNIT_3or4_15), 1)) %>%
+  mutate(UNIT_5to9_CHG = round(100 * ((UNIT_5to9 - UNIT_5to9_15)/UNIT_5to9_15), 1)) %>%
+  mutate(UNIT_1019_CHG = round(100 * ((UNIT_1019 - UNIT_1019_15)/UNIT_1019_15), 1)) %>%
+  mutate(UNIT_2049_CHG = round(100 * ((UNIT_2049 - UNIT_2049_15)/UNIT_2049_15), 1)) %>%
+  mutate(UNIT_50P_CHG = round(100 * ((UNIT_50P - UNIT_50P_15)/UNIT_50P_15), 1))
+
+
+write.csv(change_df, "U:\\FY2022\\Planning\\RegionalHousingInitiative\\SubmarketAnalysis\\data\\LPA_Test3_Submarkets\\submarket_chg_2015_2020.csv")
---
+++ R/PullCensusData_County_UnitsInStructure.R
@@ -0,0 +1,107 @@
+# Author: Brian Carney
+# Last updated: 03/16/2023
+# Prepared for: Region Housing Initiative - Submarket Clustering Analysis
+
+# Set working directory
+setwd("C:\\Users\\bcarney\\Documents\\GitHub\\housing_initiative_submarkets\\R")
+
+#remove.packages("tidycensus")
+#install.packages("tidycensus", dependencies = TRUE)
+
+#remove.packages("dplyr")
+#install.packages("dplyr", dependencies = TRUE)
+
+# Install necessary packages
+#install.packages("censusapi")
+#install.packages("tidyverse")
+#install.packages("dplyr")
+#install.packages("rgdal")
+#install.packages("tidycensus")
+#update.packages("tidycensus")
+
+
+# Load packages
+library(tidyverse)
+library(dplyr)
+library(rgdal)
+library(tidycensus)
+library(censusapi)
+
+# Load Census API key
+census_api_key("e6cd8f90ccb0acacdfa5373911a2e73b96dbd792", install=TRUE, overwrite=TRUE)
+readRenviron("~/.Renviron")
+
+
+# Variable List - For Reference
+acs5_varlist <- load_variables(year = 2015,
+                                    dataset = "acs5",
+                                    cache = TRUE)
+
+
+# ---- Define Variables ----
+acs5_vars_tot <- c(
+  # Units in Structure (Occupied Housing Units)
+  UNITS_STR = "B25024_001",
+  UNIT_1DET = "B25024_002",
+  UNIT_1ATT = "B25024_003",
+  UNIT_2 = "B25024_004",
+  UNIT_3or4 = "B25024_005",
+  UNIT_5to9 = "B25024_006",
+  UNIT_1019 = "B25024_007",
+  UNIT_2049 = "B25024_008",
+  UNIT_50P = "B25024_009",
+  UNIT_MOB = "B25024_010")
+
+dvrpc_states <- c(34, 42)
+dvrpc_fips <- c('^34005|^34007|^34015|^34021|^42017|^42029|^42045|^42091|^42101')
+
+raw_data_11 <- get_acs(geography = "county",
+                       variables = acs5_vars_tot,
+                       year = 2011,
+                       state = dvrpc_states,
+                       survey = "acs1",
+                       output = "wide"
+) %>%
+  filter(str_detect(GEOID, dvrpc_fips)) %>%
+  select(GEOID, ends_with("E"), -NAME)%>%
+  `colnames<-`(str_replace(colnames(.),"E$","")) %>%
+  rename("UNITS_STR_11" = "UNITS_STR") %>%
+  rename("UNIT_1DET_11" = "UNIT_1DET") %>%
+  rename("UNIT_1ATT_11" = "UNIT_1ATT") %>%
+  rename("UNIT_2_11" = "UNIT_2") %>%
+  rename("UNIT_3or4_11" = "UNIT_3or4") %>%
+  rename("UNIT_5to9_11" = "UNIT_5to9") %>%
+  rename("UNIT_1019_11" = "UNIT_1019") %>%
+  rename("UNIT_2049_11" = "UNIT_2049") %>%
+  rename("UNIT_50P_11" = "UNIT_50P") %>%
+  rename("UNIT_MOB_11" = "UNIT_MOB")
+
+raw_data_21 <- get_acs(geography = "county",
+                       variables = acs5_vars_tot,
+                       year = 2021,
+                       state = dvrpc_states,
+                       survey = "acs1",
+                       output = "wide"
+) %>%
+  filter(str_detect(GEOID, dvrpc_fips)) %>%
+  select(GEOID, ends_with("E"), -NAME)%>%
+  `colnames<-`(str_replace(colnames(.),"E$",""))
+
+
+chg_df <- left_join(raw_data_11, raw_data_21, by="GEOID") %>%
+  select(-GEOID) %>%
+  summarise(across(everything(), sum)) %>%
+  mutate(UNITS_STR_CHG = round((UNITS_STR - UNITS_STR_11)/UNITS_STR_11, 3)) %>%
+  mutate(UNIT_1DET_CHG = round((UNIT_1DET - UNIT_1DET_11)/UNIT_1DET_11, 3)) %>%
+  mutate(UNIT_1ATT_CHG = round((UNIT_1ATT - UNIT_1ATT_11)/UNIT_1ATT_11, 3)) %>%
+  mutate(UNIT_2_CHG = round((UNIT_2 - UNIT_2_11)/UNIT_2_11, 3)) %>%
+  mutate(UNIT_3or4_CHG = round((UNIT_3or4 - UNIT_3or4_11)/UNIT_3or4_11, 3)) %>%
+  mutate(UNIT_5to9_CHG = round((UNIT_5to9 - UNIT_5to9_11)/UNIT_5to9_11, 3)) %>%
+  mutate(UNIT_1019_CHG = round((UNIT_1019 - UNIT_1019_11)/UNIT_1019_11, 3)) %>%
+  mutate(UNIT_2049_CHG = round((UNIT_2049 - UNIT_2049_11)/UNIT_2049_11, 3)) %>%
+  mutate(UNIT_50P_CHG = round((UNIT_50P - UNIT_50P_11)/UNIT_50P_11, 3)) %>%
+  mutate(UNIT_MOB_CHG = round((UNIT_MOB - UNIT_MOB_11)/UNIT_MOB_11, 3))
+
+
+write.csv(chg_df, "U:\\FY2022\\Planning\\RegionalHousingInitiative\\SubmarketAnalysis\\data\\units_in_structure_region_2011_2021.csv")
+  
