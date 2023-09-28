66459d90bd6a3fa98f8aee76615e27471bd173c7
brcarney <bcarney1919@gmail.com>
2023-09-28 10:33:39 -0400

    refactor: added shapefile export and table summary

--- python/overlays/planningareas_overlay.py
+++ python/overlays/planningareas_overlay.py
@@ -1,5 +1,6 @@
 import sys
 import pandas as pd
+import numpy as np
 import geopandas as gpd
 from sqlalchemy import create_engine
 
@@ -12,7 +13,7 @@ engine = create_engine(
 
 submarkets_planningareas = pd.read_sql(
     """
-    select sr."GEOID", sr."Class", sr.geometry, lrp.pa_2050
+    select sr."GEOID", CAST(sr."Class" as char(1)), sr.geometry, lrp.pa_2050
     from submarkets.submarket_results sr
     join public.lrp_2050_planningareas lrp
     on ST_Intersects(st_centroid(sr.geometry), lrp.shape)
@@ -20,9 +21,26 @@ submarkets_planningareas = pd.read_sql(
     engine,
 )
 
+sql = """
+    select sr."GEOID", CAST(sr."Class" as char(1)), sr.geometry, lrp.pa_2050
+    from submarkets.submarket_results sr
+    join public.lrp_2050_planningareas lrp
+    on ST_Intersects(st_centroid(sr.geometry), lrp.shape)
+    """
+
+
+gdf = gpd.read_postgis(sql, engine, geom_col="geometry")
+gdf.to_file(
+    "U:\\FY2022\Planning\\RegionalHousingInitiative\\SubmarketAnalysis\\shapefiles\\overlays\\submarkets_planningareas.shp",
+    driver="ESRI Shapefile",
+)
+
 
 submarkets_planningareas = submarkets_planningareas[["Class", "pa_2050"]]
 submarkets_planningareas["count"] = ""
 
-test = submarkets_planningareas.groupby("Class", "pa_2050").count()
-print(test)
+groupby = submarkets_planningareas.groupby(["Class", "pa_2050"]).count()
+
+groupby.to_excel(
+    "U:\\FY2022\\Planning\\RegionalHousingInitiative\\SubmarketAnalysis\\data\\overlays\\submarkets_planningareas_summary.xlsx"
+)
