8ee6f2af58f85c7469883fd9a8f45ad010e6fcf6
brcarney <44352367+brcarney@users.noreply.github.com>
2026-08-19 09:25:09 -0400

    calculated subsidized units by tract for updated nhpd db

--- sql/region_properties_ph_s8_lihtc.sql
+++ sql/region_properties_ph_s8_lihtc.sql
@@ -1,7 +1,25 @@
-DROP TABLE IF EXISTS nhpd.region_properties_ph_s8_lihtc;
-CREATE TABLE nhpd.region_properties_ph_s8_lihtc AS
-SELECT * FROM nhpd."102022_nhpd"
-WHERE "County Code" IN (34005, 34007, 34015, 34021, 42017, 42029, 42045, 42091, 42101) AND
-("NumberActiveSection8" > 0 OR
-"NumberActiveLihtc" > 0 OR
-"NumberActivePublicHousing" > 0);
+drop materialized view if exists nhpd.total_subsidized_units_by_tract cascade;
+
+create materialized view nhpd.total_subsidized_units_by_tract as 
+
+select
+	geoid,
+	sum(coalesce("S8_1_AssistedUnits",
+	0) + coalesce("S8_2_AssistedUnits",
+	0)) as total_s8,
+	sum((coalesce("PH_1_AssistedUnits",
+	0) + coalesce("PH_2_AssistedUnits",
+	0))) as total_ph,
+	sum((coalesce("LIHTC_1_AssistedUnits",
+	0) + coalesce("LIHTC_2_AssistedUnits",
+	0))) as total_lihtc,
+	sum(coalesce("S8_1_AssistedUnits",
+	0) + coalesce("S8_2_AssistedUnits", 0) + coalesce("LIHTC_1_AssistedUnits",
+	0) + coalesce("LIHTC_2_AssistedUnits", 0) + coalesce("LIHTC_1_AssistedUnits",
+	0) + coalesce("LIHTC_2_AssistedUnits", 0)) as total_subsidized_units
+from
+	nhpd.region_subsidizedhousing_tracts
+where
+		left(geoid,
+		5) in ('34005', '34007', '34015', '34021', '42017', '42029', '42045', '42091', '42101')
+group by geoid
--- sql/region_subsidizedhousing_tracts.sql
+++ sql/region_subsidizedhousing_tracts.sql
@@ -1,9 +1,22 @@
-drop table if exists nhpd.region_subsidizedhousing_tracts;
-create table nhpd.region_subsidizedhousing_tracts as 
+--drop materialized view if exists nhpd.region_subsidizedhousing_tracts;
+create materialized view nhpd.region_subsidizedhousing_tracts as 
+with nhpd_geom as (
 select
-    a.*,
-    tracts.geoid
+	*,
+	st_setsrid(st_makepoint("Longitude",
+	"Latitude"),
+	4326) as geom
 from
-    nhpd.region_properties_ph_s8_lihtc a
-join census.census_tracts_2020 tracts on
-    st_intersects(tracts.shape, st_transform(a.geom, 26918));
+	nhpd."20260818_nhpd"
+where
+	left("CensusTract",
+	5) in ('34005', '34007', '34015', '34021', '42017', '42029', '42045', '42091', '42101'))
+	select
+		n.*,
+		tracts.geoid
+	from
+		nhpd_geom n
+	join census.census_tracts_2020 tracts on
+		st_intersects(tracts.shape,
+		st_transform(n.geom,
+		26918));
