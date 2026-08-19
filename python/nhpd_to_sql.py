2782712d9899f0bc04108f4b00f0b74de6c20f4e
brcarney <44352367+brcarney@users.noreply.github.com>
2026-08-19 09:24:45 -0400

    added nhpd to dw postgres to test

--- python/nhpd_to_sql.py
+++ python/nhpd_to_sql.py
@@ -1,14 +1,17 @@
 import pandas as pd
 import sqlalchemy
 import psycopg2
+import openpyxl
+from settings import dw_creds
 
 from sqlalchemy import create_engine
 
-engine = create_engine("postgresql://postgres:dvrpc@localhost:5433/housing_initiative")
+engine = create_engine(dw_creds)
 
 
 nhpd = pd.read_excel(
-    "G:\\Shared drives\\FY22 Regional Housing Initiative\\Data\\NHPD\\20220830_download\\Active Properties.xlsx"
+    "G:/Shared drives/Planning Innovation/Support/FY2027/housing_submarkets/nhpd_20260818.xlsx",
+    dtype={"Zip": str, "CBSACode": str, "CountyCode": str, "CensusTract": str},
 )
 
-nhpd.to_sql("102022_nhpd", engine, schema="nhpd")
+nhpd.to_sql("20260818_nhpd", engine, schema="nhpd", if_exists="replace")
