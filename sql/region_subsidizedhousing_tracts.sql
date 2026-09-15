3b0d96ac72ff8e74905765dcf9de6ce57e06d256
brcarney <44352367+brcarney@users.noreply.github.com>
2026-09-15 15:06:39 -0400

    feat: calculated subidized housing units by tract

--- sql/region_subsidizedhousing_tracts.sql
+++ sql/region_subsidizedhousing_tracts.sql
@@ -1,22 +1,22 @@
---drop materialized view if exists nhpd.region_subsidizedhousing_tracts;
-create materialized view nhpd.region_subsidizedhousing_tracts as 
+--drop materialized view if exists rhi.region_subsidizedhousing_tracts;
+create materialized view rhi.region_subsidizedhousing_tracts as 
 with nhpd_geom as (
 select
 	*,
-	st_setsrid(st_makepoint("Longitude",
-	"Latitude"),
+	st_setsrid(st_makepoint("longitude",
+	"latitude"),
 	4326) as geom
 from
-	nhpd."20260818_nhpd"
+	rhi."20260818_nhpd"
 where
-	left("CensusTract",
+	left("censustract",
 	5) in ('34005', '34007', '34015', '34021', '42017', '42029', '42045', '42091', '42101'))
 	select
 		n.*,
 		tracts.geoid
 	from
 		nhpd_geom n
-	join census.census_tracts_2020 tracts on
+	join rhi.census_tracts_2020 tracts on
 		st_intersects(tracts.shape,
 		st_transform(n.geom,
 		26918));
--- sql/region_subsidizedhousingunits_bytract.sql
+++ sql/region_subsidizedhousingunits_bytract.sql
@@ -1,9 +1,9 @@
-drop table if exists nhpd.region_subsidizedhousing_unitsbytract;
-create table nhpd.region_subsidizedhousing_unitsbytract as
+drop materialized view if exists rhi.region_subsidizedhousing_unitsbytract;
+create materialized view rhi.region_subsidizedhousing_unitsbytract as
 select
 	tracts.geoid,
-	sum("Total Units") as "Total Subsidized Units"
-from  census.census_tracts_2020 tracts
-left join nhpd.region_subsidizedhousing_tracts a on
+	sum("totalunits") as "Total Subsidized Units"
+from  rhi.census_tracts_2020 tracts
+left join rhi.region_subsidizedhousing_tracts a on
 	tracts.geoid = a.geoid
 group by tracts.geoid;
